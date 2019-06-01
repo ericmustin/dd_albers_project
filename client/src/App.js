8067483afb13567d8982df109f14ee18ae5b147c
ericmustin <mustin.eric@gmail.com>
2019-06-01 12:31:50 +0200

    adds basic avg and sum toggle

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,11 @@ class AlbersUSA extends Component {
       sorting_key: '',
       aggregation: ''
     }
+
+    this.switchToAvg = this.switchToAvg.bind(this)
+    this.switchToSum = this.switchToSum.bind(this)
   }
+
   componentWillMount() {
     this.callBackendAPI().then( (res) => {
       this.setState({ logs_api_output: res.logs || [], sorting_key: res.sorting_key || 'revenue', aggregation: res.aggregation || 'sum' })
@@ -49,80 +53,98 @@ class AlbersUSA extends Component {
     return body;
   }
 
+  switchToAvg() {
+    this.setState({ aggregation: "avg" })
+  }
+
+  switchToSum() {
+    this.setState({ aggregation: "sum" })
+  }
+
   render() {
     const logs_api_output = this.state.logs_api_output
     const sorting_key = this.state.sorting_key
     const aggregation = this.state.aggregation
 
     return (
-      <div style={wrapperStyles}>
-        <ComposableMap
-          projection="albersUsa"
-          projectionConfig={{
-            scale: 1000,
-          }}
-          width={980}
-          height={551}
-          style={{
-            width: "100%",
-            height: "auto",
-          }}
-          >
-          <ZoomableGroup disablePanning>
-            <Geographies geography={us_json.default} disableOptimization>
-              {(geographies, projection) => {
-                
-                let max = Math.max.apply(null,logs_api_output.map( (x) => { return x.content.attributes[sorting_key] }))
-                if(max < 0) {
-                  max = 0
-                }
-
-                colorScale = scaleLinear().domain([10,max]).range(["#FFFFFF","#774aa4"])
-
-                const sorted_logs_api_output = logs_api_output.reduce( (storage,log) => {
-                  let name = log.content.attributes.state_name
-                  if (storage[name] === undefined) {
-                    storage[name] = {sum: 0, count: 0}
-                  }
+      <div>
+        <div>
+          <button onClick={ this.switchToAvg }>
+            { "Average" }
+          </button>
+          <button onClick={ this.switchToSum }>
+            { "Sum" }
+          </button>        
+        </div>
+        <div style={wrapperStyles}>
+          <ComposableMap
+            projection="albersUsa"
+            projectionConfig={{
+              scale: 1000,
+            }}
+            width={980}
+            height={551}
+            style={{
+              width: "100%",
+              height: "auto",
+            }}
+            >
+            <ZoomableGroup disablePanning>
+              <Geographies geography={us_json.default} disableOptimization>
+                {(geographies, projection) => {
                   
-                  storage[name]['sum'] = storage[name]['sum'] + log.content.attributes[sorting_key]
-                  storage[name]['count'] = storage[name]['count'] + 1
-                  return storage
-                },{})
+                  let max = Math.max.apply(null,logs_api_output.map( (x) => { return x.content.attributes[sorting_key] }))
+                  if(max < 0) {
+                    max = 0
+                  }
 
-                return geographies.map((geography, i) => {
-                  let stateRevenue = 0
+                  colorScale = scaleLinear().domain([10,max]).range(["#FFFFFF","#774aa4"])
 
-                  if (sorted_logs_api_output[geography.properties.NAME_1] !== undefined) {
-                    if (aggregation == 'avg') {
-                      stateRevenue = sorted_logs_api_output[geography.properties.NAME_1]['sum'] /  sorted_logs_api_output[geography.properties.NAME_1]['count']
-                    } else if (aggregation == 'sum') {
-                      stateRevenue = sorted_logs_api_output[geography.properties.NAME_1]['sum']
+                  const sorted_logs_api_output = logs_api_output.reduce( (storage,log) => {
+                    let name = log.content.attributes.state_name
+                    if (storage[name] === undefined) {
+                      storage[name] = {sum: 0, count: 0}
                     }
-                  }
-                  
-                  return (
-                    <Geography
-                      key={`state-${geography.properties.ID_1}`}
-                      cacheId={`state-${geography.properties.ID_1}`}
-                      round
-                      geography={geography}
-                      projection={projection}
-                      style={{
-                        default: {
-                          fill: colorScale(+stateRevenue),
-                          stroke: "#607D8B",
-                          strokeWidth: 0.75,
-                          outline: "none"
-                        }
-                      }}
-                    />
-                  )
-                })
-              }}
-            </Geographies>
-          </ZoomableGroup>
-        </ComposableMap>
+                    
+                    storage[name]['sum'] = storage[name]['sum'] + log.content.attributes[sorting_key]
+                    storage[name]['count'] = storage[name]['count'] + 1
+                    return storage
+                  },{})
+
+                  return geographies.map((geography, i) => {
+                    let stateRevenue = 0
+
+                    if (sorted_logs_api_output[geography.properties.NAME_1] !== undefined) {
+                      if (aggregation == 'avg') {
+                        stateRevenue = sorted_logs_api_output[geography.properties.NAME_1]['sum'] /  sorted_logs_api_output[geography.properties.NAME_1]['count']
+                      } else if (aggregation == 'sum') {
+                        stateRevenue = sorted_logs_api_output[geography.properties.NAME_1]['sum']
+                      }
+                    }
+
+                    return (
+                      <Geography
+                        key={`state-${geography.properties.ID_1}`}
+                        cacheId={`state-${geography.properties.ID_1}`}
+                        round
+                        geography={geography}
+                        projection={projection}
+                        style={{
+                          default: {
+                            fill: colorScale(+stateRevenue),
+                            stroke: "#607D8B",
+                            strokeWidth: 0.75,
+                            outline: "none"
+                          }
+                        }}
+                      />
+                    )
+                  })
+                }}
+              </Geographies>
+            </ZoomableGroup>
+          </ComposableMap>
+        </div>
       </div>
     )
   }
